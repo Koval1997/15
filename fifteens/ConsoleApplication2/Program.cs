5d172b87afe324db418e98943311d0a6953a945d
Koval1997 <kovalmax06@gmail.com>
2017-03-05 11:45:04 +0500

    Update Program.cs

--- fifteens/ConsoleApplication2/Program.cs
+++ fifteens/ConsoleApplication2/Program.cs
@@ -9,14 +9,7 @@ namespace Fifteens
     class Program
     {
         static void Main(string[] args)
-        {
-            //Game field = new Game(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-            //int a = field.Dictionary[1].Y;
-            //field.Shift(1);
-            //int b = field.Dictionary[1].Y;
-            //Console.WriteLine(a);
-            //Console.WriteLine(b);
-
+        {          
             Game.FromCSV("input.csv");
         }
     }
